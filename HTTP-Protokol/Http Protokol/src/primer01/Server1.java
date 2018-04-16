68754b68be9b25f7d393baeb5b5c59cb9b65fa2e
--global <vladimir.spasojevic96@gmail.com>
2018-04-16 21:24:37 +0200

    web vezbe

---
+++ HTTP-Protokol/Http Protokol/src/primer01/Server1.java
@@ -0,0 +1,29 @@
+package primer01;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server1 {
+
+	public static final int TCP_PORT = 9000;
+
+	public static void main(String[] args) {
+		try {
+			int clientCounter = 0;
+			// slusaj zahteve na datom portu
+			@SuppressWarnings("resource")
+			ServerSocket ss = new ServerSocket(TCP_PORT);
+			System.out.println("Server running...");
+			while (true) {
+				Socket sock = ss.accept();
+				System.out.println("Client accepted: " + (++clientCounter));
+				
+				//inicijalizacija servara
+				new ServerThread(sock, clientCounter);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
